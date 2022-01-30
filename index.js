cf693c3216eaf9c445423c5429c924c1c3ba13fe
noobmaster0 <76144147+noobmaster0@users.noreply.github.com>
2022-01-29 17:38:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-let elements, html = [], script = [], css = [];
+let elements, html = [], css = [];
 const C = (tag,data) => css[css.length]=`${tag}{${data}}`;
 const H = (tag,data,properties="") => html[html.length]=`<${tag} ${properties}>${data}</${tag}>`;
 const write = (Hid="main",Sid="script",Cid="style") => {
 document.getElementById(Hid).innerHTML = html.join("");
-document.getElementById(Sid).innerHTML = script.join("");
 document.getElementById(Cid).innerHTML = css.join("");
-html = [],css = [],script = [];
+html = [],css = [];
 };
 let speed = 1000;
 function run(){
