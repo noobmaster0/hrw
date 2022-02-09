98ffc242f4ce90daed13ab6dcc68c940eda1d37c
noobmaster0 <76144147+noobmaster0@users.noreply.github.com>
2022-02-08 18:02:36 -0800

    bug fixes
    Fixed errors compacted code and set default speed to 0.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let elements, html = [], css = [];
+let elements, html = [], css = [], speed = 0;
 const C = (tag,data) => css[css.length]=`${tag}{${data}}`;
 const H = (tag,data,properties="") => html[html.length]=`<${tag} ${properties}>${data}</${tag}>`;
-const write = (Hid="main",Sid="script",Cid="style") => {
+const write = (Hid="main",Cid="style") => {
 document.getElementById(Hid).innerHTML = html.join("");
 document.getElementById(Cid).innerHTML = css.join("");
 html = [],css = [];
 };
-let speed = 1000;
 function run(){
 elements();	
+if(speed!=0){
 setInterval(elements,speed);
+}
 }
 window.onload = run;
