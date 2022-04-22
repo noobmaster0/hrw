76bdd11146e6da9a06e2b8af990413350163418d
noobmaster0 <76144147+noobmaster0@users.noreply.github.com>
2022-04-22 17:07:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let elements, html = [], css = [];
+let html = [], css = [];
 const CSS = (tag,data) => css[css.length]=`${tag}{${data}}`;
 const HTM = (tag,data,properties="") => html[html.length]=`<${tag} ${properties}>${data}</${tag}>`;
 const write = (funct,Hid="main",Cid="style") => {
